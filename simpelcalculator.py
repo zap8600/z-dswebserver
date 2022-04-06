726199de14b813586525563261f9f68aea07af09
DingusDudy <103146443+DingusDudy@users.noreply.github.com>
2022-04-06 15:47:16 -0500

    Update simpelcalculator.py

--- simpelcalculator.py
+++ simpelcalculator.py
@@ -1,38 +1,28 @@
-#
 #Addition
-#
 print('Addition:')
 first_addition = float(input('Addition First: '))
 second_addition = float(input('Addition Second: '))
 sum__ = first_addition + second_addition
 print(str(sum__))
-#
 #Subtraction
-#
 print('Subtraction:')
 first_subtract = float(input('Subtraction First: '))
 second_subtract = float(input('Subtraction Second: '))
 sum_ = first_subtract - second_subtract
 print(str(sum_))
-#
 #Multiplication
-#
 print('Multiplication:')
 first_Mult = float(input('Multiply First: '))
 second_Mult = float(input('Multiply Second: '))
 sumD = first_Mult * second_Mult
 print(str(sumD))
-#
 #Division
-#
 print('Division:')
 first_div = float(input('Division First: '))
 second_div = float(input('Division Second: '))
 sumGH = first_div / second_div
 print(str(sumGH))
-#
 #Power
-#
 print('Exponents: ')
 first_p = float(input('Number: '))
 second_p = float(input('To What Power?: '))
